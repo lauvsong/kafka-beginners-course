2593e1c58e664e45032bc919461cb2cb4128e6b3
lauvsong <wisepine@kakao.com>
2024-05-30 15:41:30 +0900

    feat: Wikimedia changes producer & handler

--- kafka-producer-wikimedia/src/main/java/io/lauvsong/demo/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/lauvsong/demo/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,8 +1,44 @@
 package io.lauvsong.demo.kafka.wikimedia;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class WikimediaChangesProducer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("key", "value");
+        properties.setProperty("bootstrap.servers", bootstrapServers);
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        // start the producer in another thread
+        eventSource.start();
+
+        // we produce for 10 minutes and block the program until then
+        TimeUnit.MINUTES.sleep(10);
     }
 }
